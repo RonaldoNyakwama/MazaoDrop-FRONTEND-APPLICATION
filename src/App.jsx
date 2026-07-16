8c47231fce02ecbd00fead2052643eff20060fe0
Ronaldo Nyakwama <nyabayonyakwama@outlook.com>
2026-07-16 03:01:14 +0300

    Add code for the why-choose-us section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Home } from "./pages/Home";
+import { WhyChooseUsSection } from "./pages/Choose"
 
 function App() {
 
   return (
     <>
       < Home />
+      < WhyChooseUsSection />
     </>
   )
 }
