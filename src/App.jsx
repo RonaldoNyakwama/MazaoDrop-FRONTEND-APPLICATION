49b97561ada4b0356c54547a6252c11a8586eb09
Ronaldo Nyakwama <nyabayonyakwama@outlook.com>
2026-07-16 04:14:19 +0300

    Add components for newsletter page.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Home } from "./pages/Home";
 import { WhyChooseUsSection } from "./pages/Choose"
 import { ShoppingSteps } from "./pages/ShoppingSteps";
 import { Testimonials } from "./pages/Testimonials";
+import { Newsletter } from "./pages/Newsletter";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       < WhyChooseUsSection />
       < ShoppingSteps />
       < Testimonials />
+      < Newsletter />
     </>
   )
 }
